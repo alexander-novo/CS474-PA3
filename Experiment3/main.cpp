8e4bc22e5488b5410f845e32d7e9212b14f5536d
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2020-11-15 13:10:44 -0800

    Actually finished merge

--- Experiment3/main.cpp
+++ Experiment3/main.cpp
@@ -1,80 +1,9 @@
 // Experiment3/main.cpp
-#include <algorithm>
 #include <cmath>
 #include <complex>
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <iterator>
-#include <sstream>
-#include <string>
-
-#include "../Common/fft.h"
-
-/* @Brief: Removes either the magnitude or phase of a Fourier Transofrmed image
-   @Param: image - the input image
-   @Param: comp - the flag for which component to keep, 0 keeps the magnitude,
-                  1 keeps the phase.
-   @Return: void
-*/
-void set_mag_phase(Image& image, bool comp) {
-	float real_values[image.cols * image.rows];
-	float imag_values[image.cols * image.rows] = {
-	    0};  // intitializae imaginary part to zero
-
-	// get real part from image
-	for (int i = 0; i < image.cols; i++) {
-		for (int j = 0; j < image.rows; j++) {
-			real_values[i * image.cols + j] = image[i][j];
-		}
-	}
-
-	// perform the 2D DFT
-	fft2D(image.cols, image.rows, real_values, imag_values, -1);
-
-	// Keep magnitude component
-	if (comp == 0) {
-		for (int i = 0; i < image.cols * image.rows; i++) {
-			real_values[i] = sqrt(pow(real_values[i], 2) + pow(imag_values[i], 2));
-			imag_values[i] = 0;
-		}
-	}
-
-	// Keep phase component
-	else {
-		for (int i = 0; i < image.cols * image.rows; i++) {
-			float theta    = atan2(imag_values[i], real_values[i]);
-			real_values[i] = cos(theta);
-			imag_values[i] = sin(theta);
-		}
-	}
-
-	// inverse 2D DFT
-	fft2D(image.cols, image.rows, real_values, imag_values, 1);
-
-	float max = 0.0;
-	float min = 1000000.0;
-	float value;
-
-	// get max and min values for normalization
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
-			value = real_values[i * image.rows + j];
-
-			if (value > max) { max = value; }
-			if (value < min) min = value;
-		}
-	}
-
-	// scale each pixel and update final image
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
-			double scaled_value = 255.0 * ((real_values[i * image.cols + j] - min) /
-			                               (double) (max - min));
-			image[i][j]         = (int) scaled_value;
-		}
-	}
-}
 
 #include "../Common/fft.h"
 
